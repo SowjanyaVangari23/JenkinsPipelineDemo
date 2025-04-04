de538302f1ddaff9dca3905da9f2515d4786a513
Sowjanya Vangari <166907763+SowjanyaVangari23@users.noreply.github.com>
2025-04-04 12:08:30 +0530

    Delete SpringTask directory

--- SpringTask/src/main/java/com/ig/SpringTask/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.ig.SpringTask.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
